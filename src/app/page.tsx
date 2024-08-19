4bec5fc053fddf90701d383cca4386ea5d765bd1
NeilRow56 <neilrowland56@gmail.com>
2024-08-19 09:04:23 +0100

    Added next auth bcryptjs and created login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { lusitana } from "@/components/shared/fonts";
 import Image from "next/image";
 import AppLogo from "@/components/shared/app-logo";
+import { ArrowRightIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
@@ -16,19 +18,21 @@ export default function Page() {
           >
             <strong>Welcome to Next 15 Admin Dashboard.</strong>
           </p>
-
-          <Link href="/login">
-            <span>Log in</span>
-            {/* <ArrowRightIcon className="w-6" /> */}
-          </Link>
+          <Button asChild className="w-1/2">
+            <Link href="/login">
+              <span>Log in</span>
+              <ArrowRightIcon className="w-6" />
+            </Link>
+          </Button>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
+            priority
             alt="Screenshots of the dashboard project showing desktop version"
-            className="hidden md:block"
+            className="hidden md:block w-auto"
           />
           <Image
             src="/hero-mobile.png"
